4288e934650673a000c969eed67af7d2a2d9e25f
Jesse Stricker <me@jessestricker.de>
2020-03-07 22:48:23 +0100

    Add record builder and macros

--- src/record.cpp
+++ src/record.cpp
@@ -1,1 +1,23 @@
 #include "record.hpp"
+
+#include "logger.hpp"
+
+namespace logr {
+  RecordBuilder::RecordBuilder(Logger& logger, Level level, const meta::SourceLocation& src_loc)
+      : logger_{logger}
+      , level_{level}
+      , src_loc_{src_loc} {
+  }
+
+  RecordBuilder::~RecordBuilder() noexcept {
+    auto rec = Record{};
+    rec.level = level_;
+    rec.src_loc = src_loc_;
+    rec.message = msg_buffer_.str();
+    logger_.append(rec);
+  }
+
+  RecordBuilder do_log(Logger& logger, Level level, const meta::SourceLocation& src_loc) {
+    return {logger, level, src_loc};
+  }
+}
